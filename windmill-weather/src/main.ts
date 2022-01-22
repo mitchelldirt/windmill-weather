524e5c90d5dc805aaa663a40e4288e8797f023ac
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-21 22:30:50 -0500

    got the open weather api to pull in data then broke something in my config

--- windmill-weather/src/main.ts
+++ windmill-weather/src/main.ts
@@ -1,2 +1,24 @@
-import '../dist/output.css'
+import '../dist/assets/index.42a6aa9c.css'
+const submitBtn = document.getElementById("submitBtn") as HTMLButtonElement;
+const searchBar = document.getElementById("searchBar") as HTMLInputElement;
+
+submitBtn.onclick = (e) => {
+  e.preventDefault();
+  getWeather(searchBar)
+};
+
+async function getWeather(input: HTMLInputElement): Promise<void> {
+  try {
+  let output = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=79994613e7af015836a5a0e8225ca668`, 
+  { mode: "cors" }
+  );
+  if (output.status === 200) {
+    let data = await output.json();
+    console.log(data)
+  }
+}
+catch(err) {
+  console.log(err)
+}
+}
 
