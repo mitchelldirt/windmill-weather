0d89e3c21b1b92f00e24b1779edf3b1c3edaeb0f
Mitchell Mudd <mitchellmudd@outlook.com>
2022-01-22 20:03:42 -0500

    Was able to print out temperature from on click

--- windmill-weather/src/main.ts
+++ windmill-weather/src/main.ts
@@ -1,24 +1,30 @@
-import '../dist/assets/index.42a6aa9c.css'
+import './output.css'
 const submitBtn = document.getElementById("submitBtn") as HTMLButtonElement;
 const searchBar = document.getElementById("searchBar") as HTMLInputElement;
+let data : JSON;
 
 submitBtn.onclick = (e) => {
   e.preventDefault();
-  getWeather(searchBar)
+  getWeather(searchBar).then(Response => {
+    console.log(Response.main.temp);
+  })
 };
 
-async function getWeather(input: HTMLInputElement): Promise<void> {
+async function getWeather(input: HTMLInputElement): Promise<any> {
   try {
   let output = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=79994613e7af015836a5a0e8225ca668`, 
   { mode: "cors" }
   );
   if (output.status === 200) {
-    let data = await output.json();
+    data = await output.json();
+    console.log(data.main.temp);
     console.log(data)
+    return data;
   }
 }
 catch(err) {
   console.log(err)
+  return;
 }
 }
 
